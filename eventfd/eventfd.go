9cf3a56b6b2b5fc088325c600a70ee377458853d
yushoyamaguchi <ysh.824@outlook.jp>
2025-01-16 14:07:27 +0900

    eventfd

--- eventfd/eventfd.go
+++ eventfd/eventfd.go
@@ -1,6 +1,7 @@
 package eventfd
 
 import (
+	"encoding/binary"
 	"syscall"
 )
 
@@ -14,4 +15,24 @@ func Create() (*Eventfd, error) {
 		return nil, errno
 	}
 	return &Eventfd{int(fd)}, nil
+}
+
+func (e *Eventfd) Close() error {
+	return syscall.Close(e.fd)
+}
+
+func (e *Eventfd) Read() (uint64, error) {
+	var buf [8]byte
+	_, err := syscall.Read(e.fd, buf[:])
+	if err != nil {
+		return 0, err
+	}
+	return binary.LittleEndian.Uint64(buf[:]), nil
+}
+
+func (e *Eventfd) Write(value uint64) error {
+	var buf [8]byte
+	binary.LittleEndian.PutUint64(buf[:], value)
+	_, err := syscall.Write(e.fd, buf[:])
+	return err
 }
